3c18bcbd6b9322f5dc6f08af9920bd77feae8e42
Guy Romm <guyromm@gmail.com>
2021-03-18 20:43:37 +0200

    further functions corrected.

--- sql/schema/users.VIEW.sql
+++ sql/schema/users.VIEW.sql
@@ -2,18 +2,12 @@
 --
 
 CREATE VIEW public.users AS
-SELECT
-    NULL::text AS email,
-    NULL::name AS role,
-    NULL::timestamp with time zone AS validated,
-    NULL::jsonb AS pass_reset_info,
-    NULL::jsonb AS validation_info,
-    NULL::json AS source_data,
-    NULL::json AS output_cfg,
-    NULL::json AS jobs,
-    NULL::json AS shared_source_data,
-    NULL::json AS shared_output_cfg,
-    NULL::json AS shared_jobs;
+ SELECT u.email,
+    u.role,
+    u.validated,
+    u.pass_reset_info,
+    u.validation_info
+   FROM basic_auth.users u;
 
 
 --
--- sql/schema/validate_token.cv.FUNCTION.sql
+++ sql/schema/validate_token.cv.FUNCTION.sql
@@ -51,8 +51,4 @@ end;
 $$;
 
 
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
 --
