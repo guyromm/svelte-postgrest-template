49249a9a8e687586736a886bab207eaa3ecee68a
Guy Romm <guyromm@gmail.com>
2021-10-26 11:11:50 +0300

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-session=`whoami`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $DIR/.env
+session="$APPNAME"
 
 function ti() {
     #tmux -d "$@" -s $session
